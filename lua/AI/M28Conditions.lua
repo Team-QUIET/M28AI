bb935eaa86f69995d51412c7f587f39ca0190142
Azraeel <kanyepayton1@gmail.com>
2025-11-29 20:43:04 +0900

    Turn Off Debug Whoops

--- lua/AI/M28Conditions.lua
+++ lua/AI/M28Conditions.lua
@@ -1530,7 +1530,7 @@ function ZoneWantsT1Spam(tLZTeamData, iTeam)
 end
 
 function WantMoreFactories(iTeam, iPlateau, iLandZone, bIgnoreMainEcoConditions)
-    local bDebugMessages = true if M28Profiler.bGlobalDebugOverride == true then   bDebugMessages = true end
+    local bDebugMessages = false if M28Profiler.bGlobalDebugOverride == true then   bDebugMessages = true end
     local sFunctionRef = 'WantMoreFactories'
     M28Profiler.FunctionProfiler(sFunctionRef, M28Profiler.refProfilerStart)
 
