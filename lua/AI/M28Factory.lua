26670543d1334a2572b62638172536cf05b775e9
Azraeel <kanyepayton1@gmail.com>
2025-12-11 21:42:24 +0900

    Fix Missed Maudlin Change

--- lua/AI/M28Factory.lua
+++ lua/AI/M28Factory.lua
@@ -2670,11 +2670,11 @@ function GetBlueprintToBuildForLandFactory(aiBrain, oFactory)
                     end
                     for _, iAdjLZ in tLZData[M28Map.subrefLZAdjacentLandZones] do
                         if iFactoryTechLevel < 3 or not (bDontConsiderBuildingMAA) then
-                            iCategoryToGet = GetLandZoneSupportCategoryWanted(oFactory, iTeam, tLZTeamData, iPlateau, iLandZone, iAdjLZ, bDontConsiderBuildingMAA, bConsiderMobileShields, bConsiderMobileStealths, bConsiderAbsolvers, (true and GetGameTimeSeconds() >= 21*60 and bDontGetCombat),nil,bDontConsiderLandScouts, bDontConsiderBuildingSkirmishers)
+                            iCategoryToGet = GetLandZoneSupportCategoryWanted(oFactory, iTeam, tLZTeamData, iPlateau, iLandZone, iAdjLZ, bDontConsiderBuildingMAA, bConsiderMobileShields, bConsiderMobileStealths, bConsiderAbsolvers,bDontGetCombat,nil,bDontConsiderLandScouts, bDontConsiderBuildingSkirmishers)
                         elseif M28Team.tTeamData[iTeam][M28Team.refiEnemyAirToGroundThreat] >= 1000 then
-                            iCategoryToGet = GetLandZoneSupportCategoryWanted(oFactory, iTeam, tLZTeamData, iPlateau, iLandZone, iAdjLZ, false, bConsiderMobileShields, bConsiderMobileStealths, bConsiderAbsolvers,(true and GetGameTimeSeconds() >= 21*60 and bDontGetCombat),nil,bDontConsiderLandScouts, bDontConsiderBuildingSkirmishers)
+                            iCategoryToGet = GetLandZoneSupportCategoryWanted(oFactory, iTeam, tLZTeamData, iPlateau, iLandZone, iAdjLZ, false, bConsiderMobileShields, bConsiderMobileStealths, bConsiderAbsolvers,bDontGetCombat,nil,bDontConsiderLandScouts, bDontConsiderBuildingSkirmishers)
                         else
-                            iCategoryToGet = GetLandZoneSupportCategoryWanted(oFactory, iTeam, tLZTeamData, iPlateau, iLandZone, iAdjLZ, bDontConsiderBuildingMAA, bConsiderMobileShields, bConsiderMobileStealths, bConsiderAbsolvers,(true and GetGameTimeSeconds() >= 21*60 and bDontGetCombat),nil,bDontConsiderLandScouts, bDontConsiderBuildingSkirmishers)
+                            iCategoryToGet = GetLandZoneSupportCategoryWanted(oFactory, iTeam, tLZTeamData, iPlateau, iLandZone, iAdjLZ, bDontConsiderBuildingMAA, bConsiderMobileShields, bConsiderMobileStealths, bConsiderAbsolvers,bDontGetCombat,nil,bDontConsiderLandScouts, bDontConsiderBuildingSkirmishers)
                         end
                         if bDebugMessages == true then LOG(sFunctionRef..': Finished considering category to build for iAdjLZ='..iAdjLZ..'; is iCategoryToGet nil='..tostring(iCategoryToGet == nil)) end
                         if iCategoryToGet then
