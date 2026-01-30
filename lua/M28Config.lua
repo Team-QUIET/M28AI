30be661aee6985203e1bb361f05b08fe660b25b8
AzraeelianAngel <kanyepayton1@gmail.com>
2026-01-30 22:04:22 +0900

    Disable Debug

--- lua/M28Config.lua
+++ lua/M28Config.lua
@@ -4,7 +4,7 @@
 --- DateTime: 16/11/2022 07:31
 ---
 
-M28ShowUnitNames = true --Will update units to reflect the order theyve been given
+M28ShowUnitNames = false --Will update units to reflect the order theyve been given
 M28ShowEnemyUnitNames = false --Will rename enemy units to reflect their ID and lifetime count
 M28RunVeryFast = false --Game starts off at +15 speed if set to adjustable
 
@@ -18,4 +18,4 @@ M28RunGamePerformanceCheck = false --Aug 22 profiling tourney profiling - Runs r
 M28RunSimpleProfiling = false --if this is true then will output time taken every 10s
 
 --Land support debug
-M28LandSupportDebug = true --Enable detailed logs for land support selection/rally targeting
+M28LandSupportDebug = false --Enable detailed logs for land support selection/rally targeting
