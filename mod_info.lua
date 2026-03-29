9bfe6c2bfe7988e4423c1e6d78b79b2d4f1fc14c
AzraeelianAngel <kanyepayton1@gmail.com>
2026-03-29 18:31:15 +0900

    V1 QUIET

--- mod_info.lua
+++ mod_info.lua
@@ -1,9 +1,9 @@
 name = "M28AI"
-uid = "fnewm028-v290-55b4-92b6-64398e7ge43f"
-version = 290
+uid = "fnewm028quiet-v1-55b4-92b6-64398e7ge43f"
+version = 1
 copyright = "CC BY-NC-SA 4.0 Deed"
-description = "Competitive AI for FAF (and LOUD, steam and GOG) with strong macro and micro capabilities, AI variants (including M28Easy that disables most micro).  Customise how it plays through the various 'M28 prefix' game options.  Estimated as challenging for a 900-1200 global rated player. Supports campaign missions (as an AI teammate and/or in place of the default campaign AI). More info: https://forum.faforever.com/topic/5331/m28ai-devlog-v1"
-author = "maudlin27"
+description = "Maudlin28 M28 AI designed & maintained by QUIET Team"
+author = "maudlin27 & QUIET Team"
 url = ""
 icon = "/mods/M28AI/M28AI.jpg"
 selectable = true
